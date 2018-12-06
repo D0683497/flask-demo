ffc8ddbe066eaeb9d45ecf73e94514295510f30e
柯利韋 <ff129043923@gmail.com>
2018-12-07 05:10:27 +0800

    新增錯誤頁面

--- manange.py
+++ manange.py
@@ -2,17 +2,31 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
+
 
 @app.route("/login/")
 def login():
     return render_template('login.html')
 
+
 @app.route("/regist/")
 def regist():
     return render_template('regist.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
