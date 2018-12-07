a987046a938e6639684c133083c1f1088f11bb5a
柯利韋 <ff129043923@gmail.com>
2018-12-08 07:18:08 +0800

    新增登入表單

--- manange.py
+++ manange.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
+class LoginForm(FlaskForm):
+    Username = StringField(
+        'Username', 
+        validators=[DataRequired(message='不能為空!')],
+        render_kw={'class':'form-control', 'placeholder':'Username'}
+    )
+    Password = PasswordField(
+        'Password',
+        validators=[DataRequired(message='不能為空!')],
+        render_kw={'class':'form-control', 'placeholder':'Password'}
+    )
+    login = SubmitField(
+        '登入',
+        render_kw={'class':'btn btn-lg btn-primary btn-block'}
+    )
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -18,9 +39,15 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/login/")
+@app.route("/login/", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        Username = form.Username.data
+        Password = form.Password.data
+        print(Username, Password)
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
 
 @app.route("/regist/")
